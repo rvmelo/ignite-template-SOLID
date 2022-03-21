17538c33ef30ee5baff064c953d2fead202ef5e7
Roberto <robertomelo7@hotmail.com>
2022-03-21 18:19:06 -0300

    challenge finished

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,23 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const foundUser = this.usersRepository.findByEmail(email);
+
+        if (foundUser) {
+            throw new Error("Esse email já existe");
+        }
+
+        const user = this.usersRepository.create({ name, email });
+        return user;
+    }
 }
 
 export { CreateUserUseCase };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,26 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const user = this.usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new Error("Esse usuário não existe");
+        }
+
+        if (!user.admin) {
+            throw new Error("Esse usuário não é administrador");
+        }
+
+        const users = this.usersRepository.list();
+        return users;
+    }
 }
 
 export { ListAllUsersUseCase };
